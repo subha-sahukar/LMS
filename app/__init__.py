74af574b65e4ef9adc11f230a567d9559371a2ce
subha-sahukar <96671950+subha-sahukar@users.noreply.github.com>
2025-03-01 20:35:24 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from app.db import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-from app import routes
+db.init_app(app)
+
+with app.app_context():
+    # Import parts of our application
+    from app import routes, models
+    db.create_all()
