d04e940918c00f364157bc20c429c4a67513c3b5
subha-sahukar <96671950+subha-sahukar@users.noreply.github.com>
2025-03-02 10:50:13 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 
 
+#from flask import Flask
+
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'your_secret_key'
+
+#from app import routes
+
+
+
 from flask import Flask
 
+# Initialize the Flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
 
-from app import routes
+# Import the routes defined in this app
+import app.routes
