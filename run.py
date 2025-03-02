89d90b8aba877588a7faf1fc91617bdd63ab390f
subha-sahukar <96671950+subha-sahukar@users.noreply.github.com>
2025-03-02 10:47:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-from app import app
+from flask import Flask
+from app import app as application
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Initialize the Flask app with the static folder set to 'static'
+application = Flask(__name__, static_folder='static')
+
+# Import the routes defined in the app
+import app.routes
+
+if __name__ == '__main__':
+    application.run(debug=True)
